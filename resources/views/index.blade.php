b5f74bcdba6d7772b2567bfcef0e98c18554d7d8
laurens <laurenssmid@outlook.com>
2020-04-08 10:07:16 +0200

    feat: laravel and face-recognition merged

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer src="{{asset('face-recognition/face-api.min.js')}}face-api.min.js"></script>
+    <script defer src="asset('face-recognition/script.js')"></script>
+    <title>Face Recognition</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column
+        }
+        
+        img {
+            max-height: 400px;
+        }
+    </style>
+</head>
+
+<body>
+    <input type="file" id="input">
+    <img id="img" src="">
+</body>
+
+</html>
